dc78146558bfc305991e5366588d114f3360fd17
rafaelmmayer <mayer.rafa@outlook.com>
2022-05-05 02:57:59 -0300

    adicionado lógica para IMC

--- src/imc.py
+++ src/imc.py
@@ -1,3 +1,13 @@
-def calculo_imc():
-    print('calculo_imc')
-    return 0
+def calculo_imc(altura, peso):
+    imc = peso/(altura*altura)
+    return round(imc, 2)
+
+def classifica_imc(imc):
+    if imc <= 18.5:
+        return 'Abaixo do peso'
+    elif imc > 18.5 and imc <= 24.9:
+        return 'Peso normal'
+    elif imc > 25 and imc <= 29.9:
+        return 'Sobrepeso'
+    else:
+        return 'Obesidade'
--- src/main.py
+++ src/main.py
@@ -1,12 +1,33 @@
 import caloria, imc
 
+def get_input_numerico(msg):
+    input_str = input(msg)
+
+    while not input_str.replace('.', '').isdigit():
+        print('Por favor digite um número')
+        input_str = input(msg)
+    
+    return float(input_str)
+
 def main():
     '''
     Função que vai ser invocada como principal. 
-    É nela que vai ter a lógica de integraçao das outras funções auxiliares
+    É nela que vai ter a lógica de integraçao das outras funções auxiliares.
     '''
-    res_imc = imc.calculo_imc()
-    res_cal = caloria.calculo_calorias()
+    altura = get_input_numerico('\nDigite sua altura: ')
+    peso = get_input_numerico('\nDigite seu peso: ')
+
+    res_imc = imc.calculo_imc(altura, peso)
+    classificacao_imc = imc.classifica_imc(res_imc)
+
+    print('\nTabela de IMC da OMS (https://bvsms.saude.gov.br/bvs/dicas/215_obesidade.html)')
+    print(' - Menor que 18,5 - Abaixo do peso')
+    print(' - Entre 18,5 e 24,9 - Peso normal')
+    print(' - Entre 25 e 29,9 - Sobrepeso')
+    print(' - Igual ou acima de 30 - Obesidade\n')
+
+    print('Seu IMC:', res_imc)
+    print('Segundo a tabela da OMS, seu IMC está classificado como:', classificacao_imc)
 
 if __name__ == '__main__':
     main()
