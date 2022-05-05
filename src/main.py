f1a25258adfb196244b80a0e86ab5107e80eec93
rafaelmmayer <mayer.rafa@outlook.com>
2022-05-04 21:28:04 -0300

    adicionado comentário explicativo da função main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import caloria, imc
 
 def main():
+    '''
+    Função que vai ser invocada como principal. 
+    É nela que vai ter a lógica de integraçao das outras funções auxiliares
+    '''
     res_imc = imc.calculo_imc()
     res_cal = caloria.calculo_calorias()
 
